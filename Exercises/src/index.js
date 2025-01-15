04eff0a8f5987f94a94bcf80f63960be620a8748
Predu-S <181043818+Predu-S@users.noreply.github.com>
2025-01-14 23:09:08 -0300

    mudança no index.js

--- Exercises/src/index.js
+++ Exercises/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './src/Aula10.js';
+import Aula10 from './src/Aula10.js';
+import Aula11 from './src/Aula11.js';
+import Aula13 from './src/Aula13.js';
 
 
 
 ReactDOM.render(
   <Aula10 />,
+  // <Aula11 />,
+  // <Aula13 />,
   document.getElementById('root')
 );
 
